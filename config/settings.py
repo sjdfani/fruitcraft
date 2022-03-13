b3eaf1c1319ca439953451b35f945831458baced
sajjad <faniam321@gmail.com>
2022-03-13 12:29:35 +0330

    add new app in settings and urls

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'users.apps.UsersConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
+    path('account/', include('account.urls')),
 ]
