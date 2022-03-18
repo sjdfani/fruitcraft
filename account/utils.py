1165c47e3899d73fa170869692901fe65755cd9b
sajjad <faniam321@gmail.com>
2022-03-18 11:48:02 +0330

    change revive time

--- account/utils.py
+++ account/utils.py
@@ -6,5 +6,5 @@ def create_default_cards():
     if not card_exists:
         for name in ['orange', 'apple', 'banana', 'carrot']:
             Cards.objects.create(
-                name=name, card_type=CardType.COMMON, stage=1, max_power=400, revive_time='0:10 h', status=True
+                name=name, card_type=CardType.COMMON, stage=1, max_power=400, revive_time='10:00 m', status=True
             )
